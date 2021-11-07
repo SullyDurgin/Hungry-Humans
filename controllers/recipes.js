295416004dbc40a64ef5684db7e0ee992981a6ad
SullyDurgin <katsdurgin@gmail.com>
2021-11-07 15:16:18 -0500

    first page works

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -4,14 +4,109 @@ function index(req, res) {
 	Recipe.find({})
 		.then((recipes) => {
 			res.render('recipes/index', {
+				title: 'Available Recipes',
 				recipes,
-				title: 'recipe index',
 			})
 		})
-		.catch((error) => {
-			console.log(error)
+		.catch((err) => {
+			console.log(err)
 			res.redirect('/recipes')
 		})
 }
 
-export { index }
+function create(req, res) {
+	req.body.author = req.user.profile._id
+	req.body.favorite = !!req.body.favorite
+	Recipe.create(req.body)
+		.then((recipe) => {
+			res.redirect('/recipes')
+		})
+		.catch((err) => {
+			console.log(err)
+			res.redirect('/recipes')
+		})
+}
+
+function show(req, res) {
+	Recipe.findById(req.params.id)
+		.populate('author')
+		.then((recipe) => {
+			res.render('recipes/show', {
+				recipe,
+				title: 'Show Recipes',
+			})
+		})
+		.catch((err) => {
+			console.log(err)
+			res.redirect('/recipes')
+		})
+}
+
+function unfavorite(req, res) {
+	recipe
+		.findById(req.params.id)
+		.then((recipe) => {
+			recipe.favorite = !recipe.favorite
+			recipe.save().then(() => {
+				res.redirect(`/recipes/${recipe._id}`)
+			})
+		})
+		.catch((err) => {
+			console.log(err)
+			res.redirect('/recipes')
+		})
+}
+
+function edit(req, res) {
+	Recipe.findById(req.params.id)
+		.then((recipe) => {
+			res.render('recipes/edit', {
+				title: 'Edit Recipe',
+				recipe,
+			})
+		})
+		.catch((err) => {
+			console.log(err)
+			res.redirect('/recipes')
+		})
+}
+
+function update(req, res) {
+	Recipe.findById(req.params.id)
+		.then((recipe) => {
+			if (recipe.author.equals(req.user.profile._id)) {
+				// the person that created the recipe is trying to edit the recipe
+				req.body.favorite = !!req.body.favorite
+				recipe.updateOne(req.body, { new: true }).then(() => {
+					res.redirect(`/recipes/${recipe._id}`)
+				})
+			} else {
+				throw new Error('Not Your Recipe to Edit')
+			}
+		})
+		.catch((err) => {
+			console.log(err)
+			res.redirect('/recipes')
+		})
+}
+
+function deleteRecipe(req, res) {
+	Recipe.findById(req.params.id)
+		.then((recipe) => {
+			if (recipe.author.equals(req.user.profile._id)) {
+				// the person that created the recipe is trying to delete the recipe
+				recipe.delete().then(() => {
+					res.redirect('/recipes')
+				})
+			} else {
+				// the person that created the recipe is NOT the person trying to delete the recipe
+				throw new Error('Not Your Recipe to Delete')
+			}
+		})
+		.catch((err) => {
+			console.log(err)
+			res.redirect('/recipes')
+		})
+}
+
+export { index, create, show, unfavorite, edit, update, deleteRecipe as delete }
--- routes/recipes.js
+++ routes/recipes.js
@@ -1,6 +1,22 @@
 import { Router } from 'express'
 import * as recipesCtrl from '../controllers/recipes.js'
+import { isLoggedIn } from '../middleware/middleware.js'
 
 const router = Router()
+
+// localhost:3000/recipes - GET
+router.get('/', recipesCtrl.index)
+// localhost:3000/recipes/:id - GET
+router.get('/:id', recipesCtrl.show)
+// localhost:3000/recipes/:id/edit
+router.get('/:id/edit', recipesCtrl.edit)
+// localhost:3000/recipes - POST
+router.post('/', isLoggedIn, recipesCtrl.create)
+// localhost:3000/recipes/:id/flip-tasty - PATCH
+router.patch('/:id/unfavorite', isLoggedIn, recipesCtrl.unfavorite)
+// localhost:3000/recipes/:id - PUT
+router.put('/:id', isLoggedIn, recipesCtrl.update)
+// localhost:3000/recipes/:id - DELETE
+router.delete('/:id', isLoggedIn, recipesCtrl.delete)
 
 export { router }
